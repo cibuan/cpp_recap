d485bbab80b62f146232d39c97650ab77f36178a
cibuan <36897533+cibuan@users.noreply.github.com>
2018-03-07 15:24:34 +0200

    first commit
    -

---
+++ templates/templates/second.h
@@ -0,0 +1,30 @@
+#pragma once
+
+//1. Element support: the container needs to work with any data type that has a copy constructor
+//2. Memory allocation: just like arrays, the elements of the container need to be stored in continuous memory
+//3. The size of the container can grow and shrink (unlike arrays)
+//4. Avoid reallocating memory on every element insertion at the end (PushBack)
+//5. Random access using [] operator
+//6. Implement vectors interators
+//7. Make the iterators STL compatible
+//8. Support element erase
+//9. Any usability improvement
+
+template <class T1>
+class Vectrix
+{
+public:
+	// common constructor
+	Vectrix();
+	Vectrix(const T1 t);
+
+	//copy constructor
+
+	//asignment operator override
+
+	//destructor
+	~Vectrix();
+
+private:
+	T1* vectrix_array;
+};
---
+++ templates/templates/third.h
@@ -0,0 +1,10 @@
+#pragma once
+
+//Compute the Nth fibonacci number compile time
+//
+//HINTS:
+//template <int N>
+//int fibonacci();
+//
+//- use recursion
+//- use templace specialization for implementing the base case
